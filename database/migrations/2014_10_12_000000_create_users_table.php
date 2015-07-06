7a8a2d7d7b59e345e5fabf1ea23aee833e37c946
ankhuve <erik.c.forsberg@gmail.com>
2015-07-06 15:53:18 +0200

    User table migration fix

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ class CreateUsersTable extends Migration {
             $table->increments('id');
             $table->string('email')->unique();
             $table->string('password', 60);
-            $table->string('usertype');
+            $table->string('name');
             $table->rememberToken();
             $table->timestamps();
 		});
