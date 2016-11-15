8afeb955f5622b7fe9a70418ee794a92fc7402ff
Erik <erik.c.forsberg@gmail.com>
2016-11-15 11:05:33 +0100

    Utökat registreringsformulär

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -18,16 +18,6 @@
                         <form class="form-horizontal" role="form" method="POST" action="{{ url('/auth/register') }}" enctype="multipart/form-data">
                             <input type="hidden" name="_token" value="{{ csrf_token() }}">
 
-                            {{--<div class="form-group{{ $errors->has('role') ? ' has-error' : '' }}">--}}
-                            {{--<label class="col-md-4 control-label">Typ av användare</label>--}}
-                            {{--<div class="col-md-6">--}}
-                            {{--<select type="select" class="form-control" name="role">--}}
-                            {{--<option value="1">Privatperson</option>--}}
-                            {{--<option value="2">Företag</option>--}}
-                            {{--</select>--}}
-                            {{--</div>--}}
-                            {{--</div>--}}
-
                             <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
                                 <label class="col-md-4 control-label">E-Mail</label>
                                 <div class="col-md-6">
@@ -64,18 +54,85 @@
                                 </div>
                                 @if ($errors->has('cv'))
                                     <span class="help-block">
-                                            <strong>{{ $errors->first('cv') }}</strong>
-                                        </span>
+                                        <strong>{{ $errors->first('cv') }}</strong>
+                                    </span>
                                 @endif
                             </div>
 
-                            <div class="form-group">
-                                <div class="col-md-6 col-md-offset-4">
-                                    <button type="submit" class="registerFormSubmitButton col-md-12">
-                                        Registrera
-                                    </button>
+                            <hr>
+
+                            <h4 >Jag är intresserad av jobb inom..</h4>
+
+                            <div class="form-group{{ $errors->has('county') ? ' has-error' : '' }}">
+                                {!! Form::label('county', 'Län', ['class' => 'control-label col-md-4']) !!}
+                                <div class="col-md-6">
+                                    <select name="lan" class="form-control bordered" id="county" style="display: block;">
+                                        <option selected disabled>------</option>
+                                        <option value="155" label="Norge" name="Norge">Norge</option>
+                                        <option value="10" label="Blekinge län" name="Blekinge län">Blekinge län</option>
+                                        <option value="20" label="Dalarnas län" name="Dalarnas län">Dalarnas län</option>
+                                        <option value="9" label="Gotlands län" name="Gotlands län">Gotlands län</option>
+                                        <option value="21" label="Gävleborgs län" name="Gävleborgs län">Gävleborgs län</option>
+                                        <option value="13" label="Hallands län" name="Hallands län">Hallands län</option>
+                                        <option value="23" label="Jämtlands län" name="Jämtlands län">Jämtlands län</option>
+                                        <option value="6" label="Jönköpings län" name="Jönköpings län">Jönköpings län</option>
+                                        <option value="8" label="Kalmar län" name="Kalmar län">Kalmar län</option>
+                                        <option value="7" label="Kronobergs län" name="Kronobergs län">Kronobergs län</option>
+                                        <option value="25" label="Norrbottens län" name="Norrbottens län">Norrbottens län</option>
+                                        <option value="12" label="Skåne län" name="Skåne län">Skåne län</option>
+                                        <option value="1" label="Stockholms län" name="Stockholms län">Stockholms län</option>
+                                        <option value="4" label="Södermanlands län" name="Södermanlands län">Södermanlands län</option>
+                                        <option value="3" label="Uppsala län" name="Uppsala län">Uppsala län</option>
+                                        <option value="17" label="Värmlands län" name="Värmlands län">Värmlands län</option>
+                                        <option value="24" label="Västerbottens län" name="Västerbottens län">Västerbottens län</option>
+                                        <option value="22" label="Västernorrlands län" name="Västernorrlands län">Västernorrlands län</option>
+                                        <option value="19" label="Västmanlands län" name="Västmanlands län">Västmanlands län</option>
+                                        <option value="14" label="Västra Götalands län" name="Västra Götalands län">Västra Götalands län</option>
+                                        <option value="18" label="Örebro län" name="Örebro län">Örebro län</option>
+                                        <option value="5" label="Östergötlands län" name="Östergötlands län">Östergötlands län</option>
+                                        <option value="90" label="Ospecificerad arbetsort" name="Ospecificerad arbetsort">Ospecificerad arbetsort</option>
+                                    </select>
                                 </div>
                             </div>
+
+
+                            <div class="form-group{{ $errors->has('category') ? ' has-error' : '' }} m-b-2">
+                                {!! Form::label('category', 'Yrkesgrupp', ['class' => 'control-label col-md-4']) !!}
+
+                                <div class="col-md-8">
+                                    <label class="sr-only" for="job-group">Yrkesgrupp</label>
+
+
+                                    {{--Om vi vill hämta kategorierna direkt från AF--}}
+
+                                    @if (isset($searchOptions[1]))
+                                            <label class="sr-only" for="job-group">Yrkesgrupp</label>
+
+                                            @foreach($searchOptions[1]->soklista->sokdata as $option)
+                                                <div class="checkbox">
+                                                    <label>
+                                                        <input type="checkbox" name="category[]" value="{{ $option->id }}" class="input--large">
+                                                        &nbsp;{{ $option->namn }}
+                                                    </label>
+                                                </div>
+                                            @endforeach
+                                    @endif
+
+                                    @if ($errors->has('category'))
+                                        <span class="help-block">
+                                            <strong>{{ $errors->first('category') }}</strong>
+                                        </span>
+                                    @endif
+                                </div>
+                            </div>
+
+                                <div class="form-group">
+                                    <div class="col-md-6 col-md-offset-4">
+                                        <button type="submit" class="registerFormSubmitButton col-md-12">
+                                            Registrera
+                                        </button>
+                                    </div>
+                                </div>
                         </form>
                     @endif
                 </div>
