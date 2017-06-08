b4c6f14a34c01d0776581492aa51ffacefef4833
Erik Forsberg <erik.c.forsberg@gmail.com>
2017-06-08 17:05:51 +0200

    Footer implementerad

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -180,6 +180,8 @@
         <div class="view">
             @yield('content')
         </div>
+
+        @include('pages.partials.footer')
     </body>
 
     <script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
